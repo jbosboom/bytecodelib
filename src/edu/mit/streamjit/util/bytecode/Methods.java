1856b711eae5d4472a6304f9772ccb8cb89874f1
Jeffrey Bosboom <jbosboom@csail.mit.edu>
2014-10-14 17:32:16 -0400

    Methods.staticFinalFieldInitializer

--- src/edu/mit/streamjit/util/bytecode/Methods.java
+++ src/edu/mit/streamjit/util/bytecode/Methods.java
@@ -2,16 +2,22 @@ package edu.mit.streamjit.util.bytecode;
 
 import static com.google.common.base.Preconditions.checkArgument;
 import edu.mit.streamjit.util.bytecode.insts.CallInst;
+import edu.mit.streamjit.util.bytecode.insts.CastInst;
 import edu.mit.streamjit.util.bytecode.insts.LoadInst;
 import edu.mit.streamjit.util.bytecode.insts.ReturnInst;
+import edu.mit.streamjit.util.bytecode.insts.StoreInst;
 import edu.mit.streamjit.util.bytecode.types.MethodType;
+import edu.mit.streamjit.util.bytecode.types.RegularType;
 import edu.mit.streamjit.util.bytecode.types.VoidType;
 import java.lang.invoke.MethodHandle;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.EnumSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
+import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * Contains static methods for common Method operations.
@@ -19,6 +25,12 @@ import java.util.Set;
  * @since 10/3/2014
  */
 public final class Methods {
+	/**
+	 * Trampoline for initializing static final fields.  This field is public so
+	 * it can be accessed by arbitrary bytecode; it should not be modified
+	 * directly.
+	 */
+	public static final Map<String, Object> TRAMPOLINE = new ConcurrentHashMap<>();
 	private Methods() {}
 
 	/**
@@ -38,6 +50,53 @@ public final class Methods {
 		initBlock.instructions().add(new CallInst(objCtor));
 		initBlock.instructions().add(new ReturnInst(module.types().getVoidType()));
 		return init;
+	}
+
+	/**
+	 * Creates a class initializer ({@code <clinit>}) method for the given class
+	 * that initializes static final fields of the given names with the
+	 * corresponding values in the map.  The fields are of the most specific
+	 * public class type.  (TODO: allow nonpublic class types or interface types
+	 * if the best public type is Object?  general solution requires caller to
+	 * pass in the type they want)
+	 *
+	 * The generated method depends on the live field values, and so cannot be
+	 * usefully stored on disk or loaded in another VM.
+	 * @param klass the class to create a class initializer method for
+	 * @param fieldValues the values to initialize the fields with
+	 * @return the newly-created class initializer method
+	 */
+	public static Method staticFinalFieldInitializer(Klass klass, Map<String, ?> fieldValues) {
+		Module module = klass.getParent();
+		Method clinitMethod = new Method("<clinit>", module.types().getMethodType("()V"), EnumSet.of(Modifier.STATIC), klass);
+		BasicBlock clinit = new BasicBlock(clinitMethod);
+		Field trampoline = module.getKlass(Methods.class).getField("TRAMPOLINE");
+		LoadInst getstatic = new LoadInst(trampoline);
+		clinit.instructions().add(getstatic);
+
+		Method mapRemove = module.getKlass(Map.class).getMethod("remove", module.types().getMethodType(Object.class, Map.class, Object.class));
+		fieldValues.forEach((name, value) -> {
+			RegularType type = mostSpecificPublicClassType(module, value.getClass());
+			Field field = new Field(type, name, EnumSet.of(Modifier.PRIVATE, Modifier.STATIC, Modifier.FINAL), klass);
+			//use a random UUID to avoid collisions
+			String key = name+UUID.randomUUID().toString();
+			TRAMPOLINE.put(key, value);
+			CallInst remove = new CallInst(mapRemove, getstatic, module.constants().getConstant(key));
+			CastInst cast = new CastInst(field.getType().getFieldType(), remove);
+			StoreInst putstatic = new StoreInst(field, cast);
+			clinit.instructions().addAll(Arrays.asList(remove, cast, putstatic));
+		});
+
+		clinit.instructions().add(new ReturnInst(module.types().getVoidType()));
+		return clinitMethod;
+	}
+
+	private static RegularType mostSpecificPublicClassType(Module module, Class<?> c) {
+		Klass klass = module.getKlass(c);
+		while (klass.getBackingClass() != Object.class &&
+				klass.getAccess() != Access.PUBLIC)
+			klass = klass.getSuperclass();
+		return module.types().getRegularType(klass);
 	}
 
 	/**
