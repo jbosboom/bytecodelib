1543cb91c93cdce188dbe3b8010354c6bc2f9e66
Jeffrey Bosboom <jbosboom@csail.mit.edu>
2014-10-16 12:56:05 -0400

    Import more combinators from StreamJIT

--- src/edu/mit/streamjit/util/bytecode/methodhandles/Combinators.java
+++ src/edu/mit/streamjit/util/bytecode/methodhandles/Combinators.java
@@ -1,9 +1,15 @@
 package edu.mit.streamjit.util.bytecode.methodhandles;
 
 import static com.google.common.base.Preconditions.checkArgument;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.primitives.Primitives;
+import static edu.mit.streamjit.util.bytecode.methodhandles.LookupUtils.findStatic;
+import static edu.mit.streamjit.util.bytecode.methodhandles.LookupUtils.param;
 import java.lang.invoke.MethodHandle;
 import java.lang.invoke.MethodHandles;
+import java.lang.invoke.MethodHandles.Lookup;
 import java.lang.invoke.MethodType;
+import java.lang.reflect.Array;
 import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
@@ -15,6 +21,7 @@ import java.util.stream.Stream;
  * @since 10/6/2014 (some methods from StreamJIT @since 10/3/2013)
  */
 public final class Combinators {
+	private static final Lookup LOOKUP = MethodHandles.lookup();
 	private Combinators() {throw new AssertionError();}
 
 	private static final MethodHandle VOID_VOID_NOP = MethodHandles.identity(Object.class)
@@ -63,6 +70,24 @@ public final class Combinators {
 		return semicolon(handles.toArray(new MethodHandle[0]));
 	}
 
+	private static final MethodHandle METHODHANDLE_ARRAY_GETTER = MethodHandles.arrayElementGetter(MethodHandle[].class);
+	/**
+	 * Returns a MethodHandle with a leading int argument that selects one of
+	 * the MethodHandles in the given array, which is invoked with the
+	 * remaining arguments.
+	 * @param cases the cases to select from
+	 * @return a MethodHandle approximating the switch statement
+	 */
+	public static MethodHandle tableswitch(MethodHandle[] cases) {
+		checkArgument(cases.length >= 1);
+		MethodType type = cases[0].type();
+		for (MethodHandle mh : cases)
+			checkArgument(mh.type().equals(type), "Type mismatch in "+Arrays.toString(cases));
+		MethodHandle selector = METHODHANDLE_ARRAY_GETTER.bindTo(cases);
+		//Replace the index with the handle to invoke, passing it to an invoker.
+		return MethodHandles.filterArguments(MethodHandles.exactInvoker(type), 0, selector);
+	}
+
 	/**
 	 * Returns a method handle that invokes the target handle with the result of
 	 * invoking the given argument handles.
@@ -108,5 +133,74 @@ public final class Combinators {
 					a.asType(a.type().changeReturnType(target.type().parameterType(0))));
 		}
 		return target;
+	}
+
+	private static int _arraylength(boolean[] a) {
+		return a.length;
+	}
+	private static int _arraylength(byte[] a) {
+		return a.length;
+	}
+	private static int _arraylength(short[] a) {
+		return a.length;
+	}
+	private static int _arraylength(char[] a) {
+		return a.length;
+	}
+	private static int _arraylength(int[] a) {
+		return a.length;
+	}
+	private static int _arraylength(long[] a) {
+		return a.length;
+	}
+	private static int _arraylength(float[] a) {
+		return a.length;
+	}
+	private static int _arraylength(double[] a) {
+		return a.length;
+	}
+	private static int _arraylength(Object[] a) {
+		return a.length;
+	}
+	private static final MethodHandle REFERENCE_ARRAYLENGTH = findStatic(LOOKUP, "_arraylength", param(0, Object[].class));
+	private static final ImmutableMap<Class<?>, MethodHandle> PRIMITIVE_ARRAYLENGTH;
+	static {
+		ImmutableMap.Builder<Class<?>, MethodHandle> builder = ImmutableMap.builder();
+		for (Class<?> c : Primitives.allPrimitiveTypes())
+			if (c != void.class) {
+				Class<?> arrayClass = Array.newInstance(c, 0).getClass();
+				builder.put(arrayClass, findStatic(LOOKUP, Combinators.class, "_arraylength", param(0, arrayClass)));
+			}
+		PRIMITIVE_ARRAYLENGTH = builder.build();
+	}
+	/**
+	 * Returns a method handle taking one argument of the given array type and
+	 * returning its length (as an int).
+	 * @param arrayClass an array type
+	 * @return a method handle returning the length of arrays of the given type
+	 */
+	public static MethodHandle arraylength(Class<?> arrayClass) {
+		checkArgument(arrayClass.isArray(), "%s not an array class", arrayClass);
+		if (arrayClass.getComponentType().isPrimitive()) {
+			MethodHandle handle = PRIMITIVE_ARRAYLENGTH.get(arrayClass);
+			assert handle != null : arrayClass;
+			return handle;
+		}
+		return REFERENCE_ARRAYLENGTH.asType(MethodType.methodType(int.class, arrayClass));
+	}
+
+	private static final MethodHandle INTEGER_SUM = findStatic(Integer.class, "sum");
+	/**
+	 * Returns an int -> int method handle that returns the sum of its argument
+	 * and the given addend.
+	 *
+	 * TODO: it's not clear what the proper interface for math combinators is,
+	 * or if they're generally useful.  StreamJIT only needs this one (and sub
+	 * which justs uses negative addends).
+	 * @param addend the addend to add
+	 * @return a method handle that adds the given addend
+	 */
+	public static MethodHandle adder(int addend) {
+		return MethodHandles.insertArguments(INTEGER_SUM, 1, addend);
 	}
 }
